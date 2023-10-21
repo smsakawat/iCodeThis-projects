47a07850740f8ac68983eab284c0783df9e4dce6
Sakawat Hossain <sakawat.hossain.c@gmail.com>
2023-10-21 17:11:46 +0600

    dashboard updated

---
+++ management-dashboard/main.js
@@ -0,0 +1,29 @@
+const timeElem = document.getElementById('time');
+const dateElem = document.getElementById('date');
+
+const monthNames = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+
+const renderDateTime = () => {
+    const now = new Date();
+    const h = now.getHours().toString().padStart(2, '0');
+    const m = now.getMinutes().toString().padStart(2, '0');
+    const s = now.getSeconds().toString().padStart(2, '0');
+
+    const currentDay = now.getDate();
+    const currentMonth = now.getMonth();
+    const timeText = `${h}:${m}:${s}`;
+    timeElem.textContent = timeText;
+    const dateText = `${currentDay} ${monthNames[currentMonth]}`;
+    dateElem.textContent = dateText;
+}
+
+const intervalClock = () => {
+    setInterval(() => {
+        renderDateTime();
+    }, 1000);
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    renderDateTime();
+    intervalClock();
+});
