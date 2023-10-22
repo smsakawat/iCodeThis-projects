a202d8b5082d46bafe430dc34727536cf5ba8e84
Sakawat Hossain <sakawat.hossain.c@gmail.com>
2023-10-22 18:03:32 +0600

    promo project added

---
+++ promo-section/main.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const rocket = document.querySelector('.rocket');
+
+  setTimeout(() => {
+      rocket.style.transform = "scale(1)";
+  }, 300);
+});
---
+++ promo-section/readme.md
@@ -0,0 +1,5 @@
+# Promo Section
+
+It's my solution for the iCodeThis "Promo Section" challenge.
+
+[Checkout the submission here](https://icodethis.com/submissions/44623)
